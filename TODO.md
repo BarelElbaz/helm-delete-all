e5017652d00ff5fbd919fe75b3ec3359bd00dc47
Whitehawk2 <whitehawk2@gmail.com>
2021-08-25 11:15:31 +0300

    MOD: Cosmetics

--- TODO.md
+++ TODO.md
@@ -3,5 +3,6 @@
 > Whitehawk2
 
 - Add traps
+- getopt!!!
 - Add support for helm debug via $HELM_DEBUG
 - Add support for deleting all releases just in one specific NS
--- delete-all.sh
+++ delete-all.sh
@@ -66,32 +66,6 @@ do
             ;;
     esac
 done
-# --
-
-# more POSIXy(?) getopts, no "--" for now
-#while getopts ":[hH]t:" option; do
-#    case "$option" in
-#        t)  if is_integer "$OPTARG"; then
-#                echo "Setting timeout to: $OPTARG" && TIMEOUT="$OPTARG"
-#            else
-#                echo "Please use a NUMBER for timeout! (e.g, '4' for 4 secs)"
-#                exit 1
-#            fi
-#            ;;
-#       # v)  echo "Verbose mode on" && _V=1
-#       #     ;;
-#        [Hh]) display_help
-#            exit 0
-#            ;;
-#        \?) echo "Illegal option."
-#            display_help
-#            exit 1
-#            ;;
-#    esac
-#done
-
-## Get rid of the options that were processed
-#shift $((OPTIND -1))
 
 timeout "$TIMEOUT" kubectl cluster-info > /dev/null 2>&1
 test ${?} -eq 0 || handle_error "the server might be offline"
@@ -104,7 +78,7 @@ do
     echo "--------"
     helm_charts="$($HELM_BIN list -a -n ${namespace} --short)"
     if [ -z "$helm_charts" ]; then
-        echo "Namespace is empty!"
+        printf "Namespace is empty!\n"
     else
         for chart in $helm_charts ; do
             "$HELM_BIN" delete -n "${namespace}" "$chart"
